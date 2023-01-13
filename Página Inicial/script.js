4a99099e19fe8096d9f562e5a2f5f4a25823c40c
Thomaz <thomazpato@gmail.com>
2023-01-13 18:17:23 -0300

    atualização thomaz-11

--- Página Inicial/script.js
+++ Página Inicial/script.js
@@ -3,3 +3,12 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
 }
+
+
+
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
