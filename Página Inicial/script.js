63f6374ffb4a30e4ae78195deae77415a74b4f8e
Thomaz <thomazpato@gmail.com>
2023-01-13 18:52:19 -0300

    atualização thomaz-12

--- Página Inicial/script.js
+++ Página Inicial/script.js
@@ -2,9 +2,20 @@ let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    navbar.classList.remove('active');
 }
 
+let navbar = document.querySelector('.navbar');
 
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
 
 
 let header = document.querySelector('header');
